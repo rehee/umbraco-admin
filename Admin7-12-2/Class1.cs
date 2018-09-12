6f580c9cd013fe4cfb5f0bae96174d19dcf7faa6
rehee <x3iii131@gmail.com>
2018-09-12 08:17:33 +0100

    test

--- Admin7-12-2/Class1.cs
+++ Admin7-12-2/Class1.cs
@@ -17,10 +17,12 @@ namespace System
       //var query = criteria.Field("nodeTypeAlias", "home");
       //var query = criteria.Field("title", "home");
       //var query = criteria.Id(1602);
-      var query = criteria.ParentId(-1);
+      var query = criteria.Range("ParentId", -2,0);
+
       //criteria.Field("id")
       //  IExamineValue
-      var test = ExamineManager.Instance.Search("ParentId == -1",true).ToList();
+      //var test = ExamineManager.Instance.Search("ParentId == -1",true).ToList();
+      var test = ExamineManager.Instance.Search(query.Compile()).ToList();
       var a = 1;
     }
   }

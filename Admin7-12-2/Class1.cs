21faedfdbd95d634ace4b8648e39bf44c321135a
rehee <x3iii131@gmail.com>
2018-09-12 07:12:22 +0100

    add exam search

---
+++ Admin7-12-2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Examine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UmbracoExamine;
+
+namespace System
+{
+  public static class Class1
+  {
+    public static void a()
+    {
+      var criteria = ExamineManager.Instance.DefaultSearchProvider.CreateSearchCriteria(IndexTypes.Content);
+      var query = criteria.Range("Id", 0, 9999);
+      var test = ExamineManager.Instance.DefaultSearchProvider.Search(query.Compile()).ToList();
+      var a = 1;
+    }
+  }
+}
